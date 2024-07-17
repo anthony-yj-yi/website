948d321b7c94ba8a31257099e53140cb46918582
ayi6 <ayi6@myseneca.ca>
2024-07-17 18:29:48 -0400

    revert to h-screen

--- app/components/aboutMe.js
+++ app/components/aboutMe.js
@@ -1,10 +1,6 @@
 export default function AboutMe() {
-    const innerHeight = window.innerHeight;
     return(
-        <div className="w-full md:pl-[20%] pl-[10%] md:pr-[20%] pr-[10%] flex flex-col justify-center"
-        style={{
-            height: `${innerHeight}px`
-        }}>
+        <div className="w-full h-screen md:pl-[20%] pl-[10%] md:pr-[20%] pr-[10%] flex flex-col justify-center">
             <h1 className="md:text-6xl text-4xl font-bold">Hi, I'm Anthony,</h1>
             <p className="md:text-4xl text-2xl">
             a skilled programmer with a passion for web development.
