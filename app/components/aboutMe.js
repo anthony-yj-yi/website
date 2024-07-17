4ec190f5188face88019d3f23f79ed1291071bb3
ayi6 <ayi6@myseneca.ca>
2024-07-17 17:44:21 -0400

    added small dimensions for aboutMe

--- app/components/aboutMe.js
+++ app/components/aboutMe.js
@@ -1,8 +1,8 @@
 export default function AboutMe() {
     return(
-        <div className="w-full md:h-screen pl-[20%] pr-[20%] flex flex-col justify-center">
-            <h1 className="text-6xl font-bold">Hi, I'm Anthony,</h1>
-            <p className="text-4xl">
+        <div className="w-full md:h-screen md:pl-[20%] sm:pl-[5%] md:pr-[20%] sm:pr-[5%] flex flex-col justify-center">
+            <h1 className="md:text-6xl sm:text-4xl font-bold">Hi, I'm Anthony,</h1>
+            <p className="md:text-4xl sm:text-2xl">
             a skilled programmer with a passion for web development.
             Experienced in C, C++, Java, Python, and JavaScript, I create responsive web
             applications using HTML, CSS, Bootstrap, Node.js, React, and Next.js.
